eb9fb7e889004e8a2b6402244ed3dc86da6e3392
Ed Jones <edjones1999@gmail.com>
2021-04-15 20:07:33 +1000

    Added loading screen

--- src/screens/Recipes.tsx
+++ src/screens/Recipes.tsx
@@ -8,11 +8,12 @@ import { Recipes_recipes } from '../types/graphql'
 
 import { GET_RECIPES } from '../graphql/queries';
 import { RecipeCard } from '../components';
+import LoadingScreen from './LoadingScreen';
 
 export default function Recipes() {
   const { loading, error, data, refetch } = useQuery(GET_RECIPES);
 
-  if (loading) return <Spinner />;
+  if (loading) return <LoadingScreen />;
   if (error) return <Text>`Error! ${error.message}`</Text>;
 
   return (
--- src/screens/index.tsx
+++ src/screens/index.tsx
@@ -1,3 +1,4 @@
 export { default as NotFoundScreen } from './NotFoundScreen';
 export { default as Recipes } from './Recipes';
 export { default as TabTwoScreen } from './AddRecipe';
+export { default as LoadingScreen } from './LoadingScreen';

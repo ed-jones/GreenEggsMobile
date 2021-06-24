0e172afe894aeffc3fe3fae62672ebd0e5bfd093
Ed Jones <edjones1999@gmail.com>
2021-06-24 17:20:20 +1000

    Cleaned up login page

--- src/screens/auth/Login.tsx
+++ src/screens/auth/Login.tsx
@@ -1,60 +1,13 @@
 import React from 'react';
-import { View, StyleSheet, ImageBackground, Image, SafeAreaView } from 'react-native';
+import { StyleSheet } from 'react-native';
 import {
-  Text, Button, TopNavigation, TopNavigationAction, Input,
+  Text, Button, Input,
 } from '@ui-kitten/components';
-import { Icons } from '@greeneggs/core';
 import useLoginForm from './useLoginForm';
 import { setContext } from '@apollo/client/link/context';
-import { LinearGradient } from 'expo-linear-gradient';
-
-import Logo from '../../assets/images/icon.png';
-import Banner from '../../assets/images/banner.jpg';
-import { StatusBar } from 'expo-status-bar';
-import { SafeAreaInsetsContext, useSafeAreaInsets } from 'react-native-safe-area-context';
 import AuthPageTemplate from './AuthPageTemplate';
 
 const styles = StyleSheet.create({
-  logo: {
-    width: 48,
-    height: 48,
-    margin: 10
-  },
-  form: {
-    padding: 10,
-    height: "100%"
-  },
-  gradient: {
-    position: "absolute",
-    left: 0,
-    right: 0,
-    top: 0,
-    height: "100%",
-  },
-  bannerContainer: {
-    height: "25%",
-    justifyContent: "center"
-  },
-  bannerContent: {
-    alignItems: "center",
-    paddingBottom: 64,
-  },
-  banner: {
-    resizeMode: "cover",
-    height: "100%",
-    justifyContent: "flex-start",
-  },
-  view: {
-    backgroundColor: "#F7F9FC",
-  },
-  logoText: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  centerText: {
-    textAlign: "center",
-  },
   forgotPassword: {
     fontWeight: "bold",
     textAlign: "right",

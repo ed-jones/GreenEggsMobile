8df01b9e559bb3497fa77addcb00408cf46d0372
Ed Jones <edjones1999@gmail.com>
2021-07-01 18:56:00 +1000

    Implemented ControlledInput component in Login

--- src/screens/auth/Login.tsx
+++ src/screens/auth/Login.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
-import { StyleSheet } from 'react-native';
-import {
-  Button, Input, Spinner,
-} from '@ui-kitten/components';
-import useLoginForm from './useLoginForm';
-import { setContext } from '@apollo/client/link/context';
-import AuthPageTemplate from './AuthPageTemplate';
-import { Controller } from 'react-hook-form';
+import React from "react";
+import { StyleSheet } from "react-native";
+import { Button, Input, Spinner } from "@ui-kitten/components";
+import useLoginForm from "./useLoginForm";
+import { setContext } from "@apollo/client/link/context";
+import AuthPageTemplate from "./AuthPageTemplate";
+import { Controller } from "react-hook-form";
+import ControlledInput, {
+  InputType,
+} from "@greeneggs/core/controlled-input/ControlledInput";
+import { LoginInput } from "@greeneggs/types/graphql";
 
 const styles = StyleSheet.create({
   forgotPassword: {
@@ -17,11 +19,11 @@ const styles = StyleSheet.create({
   },
   input: {
     marginBottom: 10,
-  }
+  },
 });
 
 const Login = ({ navigation }: any) => {
-  const {formResult, handleSubmit, control, submitForm} = useLoginForm();
+  const { formResult, handleSubmit, control, submitForm } = useLoginForm();
 
   async function onSubmit() {
     const result = await submitForm();
@@ -30,76 +32,64 @@ const Login = ({ navigation }: any) => {
     if (token && !error) {
       setContext((_request, _previousContext) => ({
         headers: {
-          authorization: token
+          authorization: token,
         },
       }));
-      navigation.navigate('Home');
+      navigation.navigate("Home");
     }
   }
 
   return (
-    <AuthPageTemplate navigation={navigation} message="Log in to view and share recipes with your friends">
-      <Controller
-        control={control}
-        rules={{
-          maxLength: { value: 100, message: "Must be under 100 characters"},
-          required: { value: true, message: "This field is required"},
-          pattern: {
-              value: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-              message: "Not an email address"
-          }
+    <AuthPageTemplate
+      navigation={navigation}
+      message="Log in to view and share recipes with your friends"
+    >
+      <ControlledInput<LoginInput>
+        inputProps={{
+          autoFocus: true,
+          style: styles.input,
         }}
-        render={({ field: { onChange, onBlur, value }, fieldState: { error } }) => (
-          <Input
-            style={styles.input}
-            label="EMAIL"
-            onBlur={onBlur}
-            onChangeText={onChange}
-            value={value}
-            textContentType="emailAddress"
-            autoCompleteType="email"
-            autoCapitalize="none"
-            autoFocus
-            status={
-              error || formResult.data?.login.error ? "danger" : undefined
-            }
-            caption={error?.message}
-          />
-        )}
-        name="email"
-        defaultValue=""
+        controllerProps={{
+          name: "email",
+          defaultValue: "",
+          control,
+          rules: {
+            maxLength: { value: 100, message: "Must be under 100 characters" },
+            required: { value: true, message: "This field is required" },
+            pattern: {
+              value:
+                /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+              message: "Not an email address",
+            },
+          },
+        }}
+        submitError={formResult.data?.login.error}
+        type={InputType.EMAIL}
       />
-      <Controller
-        control={control}
-        rules={{
-         maxLength: { value: 100, message: "Must be under 100 characters"},
-         minLength: { value: 4, message: "Must be over 4 characters"},
-         required: { value: true, message: "This field is required"},
+      <ControlledInput<LoginInput>
+        inputProps={{
+          style: styles.input,
         }}
-        render={({ field: { onChange, onBlur, value }, fieldState: { error } }) => (
-          <Input
-            style={styles.input}
-            label="PASSWORD"
-            onBlur={onBlur}
-            onChangeText={onChange}
-            value={value}
-            textContentType="password"
-            autoCompleteType="password"
-            secureTextEntry
-            status={
-              error || formResult.data?.login.error ? "danger" : undefined
-            }
-            caption={formResult.data?.login.error?.message || error?.message}
-          />
-        )}
-        name="password"
-        defaultValue=""
+        controllerProps={{
+          name: "password",
+          defaultValue: "",
+          control,
+          rules: {
+            maxLength: { value: 100, message: "Must be under 100 characters" },
+            minLength: { value: 4, message: "Must be over 4 characters" },
+            required: { value: true, message: "This field is required" },
+          },
+        }}
+        submitError={formResult.data?.login.error}
+        type={InputType.PASSWORD}
       />
       {/* <Text category="p2" style={styles.forgotPassword}>Forgot Password?</Text> */}
       <Button
         onPress={handleSubmit(onSubmit)}
         disabled={formResult.loading}
-        accessoryLeft={formResult.loading ? () => <Spinner size='small'/> : undefined}
+        accessoryLeft={
+          formResult.loading ? () => <Spinner size="small" /> : undefined
+        }
       >
         LOGIN
       </Button>

28ee067bc83c15d953229918cade624d75182b9c
Andrew Wilkie <ChubbyFox@users.noreply.github.com>
2021-07-07 19:36:22 +1000

    Update MyProfile.tsx
    Updated such that no errors occur due to comments.
    Added Edit button and icon.
    Need to correct the avatar size and location

--- src/screens/profile/MyProfile.tsx
+++ src/screens/profile/MyProfile.tsx
@@ -1,25 +1,43 @@
 import React from 'react';
-import { Text, Image, View, StyleSheet, SafeAreaView} from 'react-native';
-import ProfileImage from '../../assets/images/banner.jpg';
+import { Image, View, StyleSheet, SafeAreaView} from 'react-native';
+import {
+  Text,
+  Button,
+  Icon
+} from '@ui-kitten/components';
+import Avatar from '../../assets/images/icon.png';
 import {
   SafeAreaInsetsContext,
   useSafeAreaInsets,
 } from "react-native-safe-area-context"
 
-
+const EditIcon = (props) => (
+  <Icon {...props} name='edit-outline'/>
+);
 
 const styles = StyleSheet.create({
-  profileImage: {
-    width: 150,
-    height: 150,
-    borderRadius: 150/2,
+  avatar: {
+    width: 120,
+    height: 120,
+    borderRadius: 120/2
   },
   view: {
-    flex: 1,
     backgroundColor: "#F7F9FC",
     alignItems: "center",
-    justifyContent: "center",
     height: "100%"
+  },
+  name: {
+    alignSelf: "flex-start"
+  },
+  description: {
+    justifyContent: "center"
+  },
+  button: {
+    marginBottom: 8,
+    alignSelf: "flex-end"
+  },
+  container: {
+    flexDirection: "row"
   }
 })
 
@@ -28,11 +46,15 @@ const MyProfile = ({ navigation }: any) => {
   const insets = useSafeAreaInsets();
   return (
     <View style={{ ...styles.view, paddingBottom: insets.bottom, paddingTop: insets.top}}>
-      <Image source={ProfileImage} style={styles.profileImage}/> // try flexbox for centering image
-      <Text>PROFILE NAME</Text>
-      <Text>Profile Description</Text>
+      <Image source={Avatar} style={styles.Avatar}/>
+      <View style={styles.container}>
+        <Text category="h5" style={styles.name}>PROFILE NAME</Text>
+        <Button style={styles.button} accessoryLeft={EditIcon}>EDIT</Button>
+      </View>
+      <Text style={styles.description} numberOfLines={2}>Profile Description</Text>
     </View>
   )
 }
+
 
 export default MyProfile;

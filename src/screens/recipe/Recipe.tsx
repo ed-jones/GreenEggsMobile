f44465c38878a3bd46bd7889efefd17dab878bb0
Ed Jones <edjones1999@gmail.com>
2021-07-07 20:42:04 +1000

    Broke down recipe view into subcomponents

--- src/screens/recipe/Recipe.tsx
+++ src/screens/recipe/Recipe.tsx
@@ -1,39 +1,24 @@
 import React from "react";
 import { useQuery } from "@apollo/client";
+import { ImageBackground, View, StyleSheet, ScrollView } from "react-native";
+import { Icons, Queries } from "@greeneggs/core";
 import {
-  ImageBackground,
-  Image,
-  View,
-  StyleSheet,
-  ScrollView,
-} from "react-native";
-import { Icons, LabelledIcon, Queries, toTitleCase } from "@greeneggs/core";
-import {
-  Card,
-  ListItem,
   Spinner,
   Text,
   TopNavigation,
   TopNavigationAction,
-  Divider,
-  List,
 } from "@ui-kitten/components";
-import {
-  recipe,
-  recipeVariables,
-  recipe_recipe_data_ingredients,
-} from "@greeneggs/types/graphql";
-import Alert from "@greeneggs/core/alert/Alert";
+import { recipe, recipeVariables } from "@greeneggs/types/graphql";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { StatusBar } from "expo-status-bar";
-import ViewMore from "@greeneggs/core/view-more/ViewMore";
 import ParallaxHeader from "@fabfit/react-native-parallax-header";
 import { LinearGradient } from "expo-linear-gradient";
-import Carousel from "react-native-snap-carousel";
-import { Dimensions } from "react-native";
 import RecipeDetailsCard from "./RecipeDetailsCard";
-import Tags, { Tag } from "../../core/tags/Tags";
-import IngredientListItem from "@greeneggs/core/ingredient-list-item/IngredientListItem";
+import RecipeAllergies from "./RecipeAllergies";
+import RecipeCategoriesTags from "./RecipeCategoriesTags";
+import RecipeIngredients from "./RecipeIngredients";
+import RecipeDirections from "./RecipeDirections";
+import RecipeTopComments from "./RecipeTopComments";
 
 const styles = StyleSheet.create({
   coverPhoto: {
@@ -121,105 +106,23 @@ const Recipe = ({ route, navigation }: any) => {
       <ScrollView>
         <View style={styles.content}>
           <RecipeDetailsCard {...recipe} navigation={navigation} />
-          {recipe.allergies.length! > 0 ? (
-            <Alert
-              type="danger"
-              message={`This recipe is unsuitable for those with allergies to ${recipe.allergies.map(
-                (allergy) => allergy.name.toLowerCase()
-              )}.`}
-            />
-          ) : null}
+          <RecipeAllergies allergies={recipe.allergies} />
           <Text category="h5" style={styles.heading}>
             Categories
           </Text>
-          <Tags
-            tags={
-              recipe.categories.map(
-                (category) =>
-                  ({
-                    name: category.name,
-                    onPress: () => null,
-                  } as Tag)
-              )!
-            }
-          />
+          <RecipeCategoriesTags categories={recipe.categories} />
           <Text category="h5" style={styles.heading}>
             Ingredients
           </Text>
-          <View style={{ marginHorizontal: -16 }}>
-            <List
-              data={recipe.ingredients!}
-              renderItem={({
-                item,
-              }: {
-                item: recipe_recipe_data_ingredients;
-              }) => <IngredientListItem ingredient={item} />}
-            />
-            <Divider />
-            <ViewMore onPress={() => null} />
-          </View>
+          <RecipeIngredients ingredients={recipe.ingredients} />
           <Text category="h5" style={styles.heading}>
             Directions
           </Text>
-          <View style={{ marginHorizontal: -16 }}>
-            <Carousel
-              sliderWidth={Dimensions.get("window").width}
-              itemWidth={Dimensions.get("window").width * 0.8}
-              data={recipe.steps}
-              renderItem={({ item }) => (
-                <Card
-                  header={() => (
-                    <Image
-                      style={{
-                        height: undefined,
-                        width: "100%",
-                        aspectRatio: 1 / 1,
-                      }}
-                      source={{
-                        uri: item.image,
-                      }}
-                    />
-                  )}
-                  footer={() => (
-                    <Text style={{ margin: 16 }}>{item.description}</Text>
-                  )}
-                >
-                  <Text category="h6">{item.title}</Text>
-                </Card>
-              )}
-            />
-          </View>
+          <RecipeDirections directions={recipe.steps} />
           <Text category="h5" style={styles.heading}>
             Top Comments
           </Text>
-          <View style={{ marginHorizontal: -16 }}>
-            <ListItem>
-              <View style={{ flexDirection: "column", padding: 10 }}>
-                <Text numberOfLines={2} style={{ marginBottom: 16 }}>
-                  Wow, I really enjoyed this recipe! If you liked this one you
-                  should check out my quiche recipe. I’ve done something similar
-                  but changed a couple of things.
-                </Text>
-                <View
-                  style={{
-                    flexDirection: "row",
-                    justifyContent: "space-between",
-                    alignItems: "center",
-                  }}
-                >
-                  <View>
-                    <Text style={{ fontWeight: "bold" }}>Bobby Rutherford</Text>
-                  </View>
-                  <View style={{ flexDirection: "row" }}>
-                    <LabelledIcon label="10" iconName="heart-outline" />
-                    <LabelledIcon label="Reply" iconName="undo-outline" />
-                  </View>
-                </View>
-              </View>
-            </ListItem>
-            <Divider />
-            <ViewMore onPress={() => null} />
-          </View>
+          <RecipeTopComments />
         </View>
       </ScrollView>
     </ParallaxHeader>

f8a95ee920270805152616089a8bff457193da25
Ed Jones <edjones1999@gmail.com>
2021-07-10 17:31:19 +1000

    Tweaked navbar primary button to look more like prototypes

--- src/core/navigation/BottomTabBar.tsx
+++ src/core/navigation/BottomTabBar.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet } from "react-native";
+import { StyleSheet, View } from "react-native";
 import {
   BottomTabBarOptions,
   BottomTabBarProps,
@@ -11,18 +11,22 @@ import {
   ThemedComponentProps,
   withStyles,
 } from "@ui-kitten/components";
+import Svg, { Circle } from "react-native-svg";
 
 const styles = StyleSheet.create({
   primary: {
-    height: 64,
-    width: 64,
+    height: 48,
+    width: 48,
+    marginLeft: 8,
+    marginTop: 8,
   },
   secondary: {
     height: 32,
     width: 32,
   },
   navbar: {
-    paddingVertical: 8,
+    paddingBottom: 24,
+    paddingTop: 12,
   },
 });
 
@@ -49,57 +53,84 @@ const BottomNavigationIcon = withStyles(
   )
 );
 
-const BottomTabBar = ({
-  navigation,
-  state,
-}: BottomTabBarProps<BottomTabBarOptions>) => (
-  <BottomNavigation
-    selectedIndex={state.index}
-    onSelect={(index) => navigation.navigate(state.routeNames[index])}
-    appearance="noIndicator"
-    style={styles.navbar}
-  >
-    <BottomNavigationTab
-      icon={() => (
-        <BottomNavigationIcon
-          name="home-outline"
-          iconStyle={IconStyle.Secondary}
-        />
-      )}
-    />
-    <BottomNavigationTab
-      icon={() => (
-        <BottomNavigationIcon
-          name="bell-outline"
-          iconStyle={IconStyle.Secondary}
-        />
-      )}
-    />
-    <BottomNavigationTab
-      icon={() => (
-        <BottomNavigationIcon
-          name="plus-circle-outline"
-          iconStyle={IconStyle.Primary}
-        />
-      )}
-    />
-    <BottomNavigationTab
-      icon={() => (
-        <BottomNavigationIcon
-          name="bookmark-outline"
-          iconStyle={IconStyle.Secondary}
-        />
-      )}
-    />
-    <BottomNavigationTab
-      icon={() => (
-        <BottomNavigationIcon
-          name="person-outline"
-          iconStyle={IconStyle.Secondary}
-        />
-      )}
-    />
-  </BottomNavigation>
+const BottomTabBar = withStyles(
+  ({
+    navigation,
+    state,
+    eva,
+  }: BottomTabBarProps<BottomTabBarOptions> & ThemedComponentProps) => (
+    <BottomNavigation
+      selectedIndex={state.index}
+      onSelect={(index) => navigation.navigate(state.routeNames[index])}
+      appearance="noIndicator"
+      style={styles.navbar}
+    >
+      <BottomNavigationTab
+        icon={(props) => (
+          <BottomNavigationIcon
+            {...props}
+            name="home-outline"
+            iconStyle={IconStyle.Secondary}
+          />
+        )}
+      />
+      <BottomNavigationTab
+        icon={(props) => (
+          <BottomNavigationIcon
+            {...props}
+            name="bookmark-outline"
+            iconStyle={IconStyle.Secondary}
+          />
+        )}
+      />
+      <BottomNavigationTab
+        icon={(props) => (
+          <View style={{ marginTop: -16 }}>
+            <Svg
+              height="72"
+              width="72"
+              style={{
+                position: "absolute",
+                marginLeft: -4,
+                marginTop: -4,
+              }}
+            >
+              <Circle
+                cx="36"
+                cy="36"
+                r="36"
+                fill={eva?.theme && eva.theme["color-success-500"]}
+              />
+            </Svg>
+            <Icon
+              {...props}
+              name="plus-outline"
+              style={styles.primary}
+              fill={eva?.theme && eva.theme["color-primary-500"]}
+            />
+          </View>
+        )}
+      />
+      <BottomNavigationTab
+        icon={(props) => (
+          <BottomNavigationIcon
+            {...props}
+            name="bell-outline"
+            iconStyle={IconStyle.Secondary}
+          />
+        )}
+      />
+      <BottomNavigationTab
+        icon={(props) => (
+          <BottomNavigationIcon
+            {...props}
+            name="person-outline"
+            iconStyle={IconStyle.Secondary}
+          />
+        )}
+      />
+    </BottomNavigation>
+  )
 );
 
 export default BottomTabBar;

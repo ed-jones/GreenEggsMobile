dca30298bc27bf59af60c9c74c9899ce150cf7c8
Victor <sharks400@gmail.com>
2021-07-16 17:06:38 +1000

    hey x

--- src/core/top-bar/TopBar.tsx
+++ src/core/top-bar/TopBar.tsx
@@ -1,15 +1,17 @@
-import React from 'react';
-import { Icon, Input } from '@ui-kitten/components';
-import { StyleSheet, View, Image } from 'react-native';
+import React from "react";
+import { Icon, Input } from "@ui-kitten/components";
+import { StyleSheet, View, Image } from "react-native";
 
-import logo512 from '../logo/logo512.png';
+import logo512 from "../logo/logo512.png";
+import { useNavigation } from "@react-navigation/core";
+import * as Icons from "../icons/Icons";
 
 const styles = StyleSheet.create({
   topNavigation: {
-    backgroundColor: 'transparent',
+    backgroundColor: "transparent",
     padding: 16,
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
   },
   icon: {
     width: 24,
@@ -26,16 +28,33 @@ const styles = StyleSheet.create({
   },
 });
 
-const TopBar = () => (
-  <View style={styles.topNavigation}>
-    <Image source={logo512} style={styles.logo} />
-    <Input
-      placeholder="Search Recipe"
-      size="large"
-      style={styles.search}
-      accessoryLeft={(props) => <Icon style={styles.icon} name="search" {...props} />}
-    />
-  </View>
-);
+export type ModeType = "search" | "home";
+
+interface ITopBar {
+  mode: ModeType;
+}
+
+const TopBar = ({ mode }: ITopBar) => {
+  const navigation = useNavigation();
+  return (
+    <View style={styles.topNavigation}>
+      {mode === "home" ? (
+        <Image source={logo512} style={styles.logo} />
+      ) : (
+        <Icons.Back style={{ width: 32, height: 32 }} fill="black" />
+      )}
+
+      <Input
+        placeholder="Search Recipe"
+        size="large"
+        style={styles.search}
+        accessoryLeft={(props) => (
+          <Icon style={styles.icon} name="search" {...props} />
+        )}
+        onBlur={() => navigation.navigate("RecipeSearch")}
+      />
+    </View>
+  );
+};
 
 export default TopBar;
--- src/screens/home/Home.tsx
+++ src/screens/home/Home.tsx
@@ -1,21 +1,55 @@
-import React from 'react';
-import { Navigation, TopBar } from '@greeneggs/core';
-import HomeTabs from './home-tabs/HomeTabs';
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import { View } from 'react-native';
+import React from "react";
+import { Navigation, TopBar } from "@greeneggs/core";
+import HomeTabs from "./home-tabs/HomeTabs";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { View } from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
+import RecipeSearch from "../recipe-search/RecipeSearch";
+import { template } from "@babel/core";
+import { NavigationContainer } from "@react-navigation/native";
+import { ModeType } from "@greeneggs/core/top-bar/TopBar";
 
+const Stack = createStackNavigator();
 
-const Home = () => { 
+interface ITopBarTemplate {
+  children: React.ReactElement;
+  mode: ModeType;
+}
+
+const TopBarTemplate = ({ children, mode }: ITopBarTemplate) => {
   const insets = useSafeAreaInsets();
 
   return (
     <>
       <View style={{ paddingTop: insets.top }}>
-        <TopBar />
+        <TopBar mode={mode} />
       </View>
-      <HomeTabs/>
+      {children}
     </>
-  )
+  );
 };
+
+const Home = () => (
+  <NavigationContainer independent>
+    <Stack.Navigator headerMode="none">
+      <Stack.Screen
+        name="HomeTabs"
+        component={() => (
+          <TopBarTemplate mode="home">
+            <HomeTabs />
+          </TopBarTemplate>
+        )}
+      />
+      <Stack.Screen
+        name="RecipeSearch"
+        component={() => (
+          <TopBarTemplate mode="search">
+            <RecipeSearch />
+          </TopBarTemplate>
+        )}
+      />
+    </Stack.Navigator>
+  </NavigationContainer>
+);
 
 export default Home;

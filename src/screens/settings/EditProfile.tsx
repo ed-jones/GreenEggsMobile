ffb6031c72156c66c71d979f5354e7bc1356fd89
Wambugu Mutahi <wm916@uowmail.edu.au>
2021-07-19 15:41:48 +1000

    no changes

--- src/screens/settings/EditProfile.tsx
+++ src/screens/settings/EditProfile.tsx
@@ -1,6 +1,67 @@
 import React from "react";
-import { Text } from "@ui-kitten/components";
+import {
+    Icon,
+    List,
+    ListItem,
+    Text,
+    TopNavigation,
+    TopNavigationAction,
+} from "@ui-kitten/components";
+import { Icons } from "@greeneggs/core";
+import { StackNavigationProp } from "@react-navigation/stack";
+import { ScrollView, StyleSheet } from "react-native";
 
-const EditProfile = () => <Text>Edit Profile</Text>;
+const styles = StyleSheet.create({
+    header: { padding: 16 },
+});
+
+interface ListItemProps {
+
+}
+
+interface EProfileListItem {
+    item: ListItemProps;
+}
+
+const EditProfileListItem = ({ item }: EProfileListItem) => (
+    <>
+        <ListItem
+        />
+    </>
+);
+
+const EditProfile = ({ navigation }: { navigation: StackNavigationProp<any> }) => {
+
+    const EditProfileItems: ListItemProps[] = [
+        {
+        },
+    ];
+
+    const navigateBack = () => {
+        navigation.goBack();
+    };
+
+    return (
+        <>
+            <TopNavigation
+                title="Edit Profile"
+                alignment="center"
+                style={{ backgroundColor: "transparent" }}
+                accessoryLeft={() => (
+                    <TopNavigationAction icon={Icons.Back} onPress={navigateBack} />
+                )}
+            />
+            <ScrollView>
+                <Text category="h6" style={styles.header}>
+                    Profile Picture
+                </Text>
+                <Text category="h6" style={styles.header}>
+                    Profile Details
+                </Text>
+            </ScrollView>
+
+        </>
+    );
+};
 
 export default EditProfile;

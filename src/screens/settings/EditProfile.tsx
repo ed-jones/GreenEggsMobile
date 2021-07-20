7a787623591d88978acc31d7bebce890d28cf67a
Wambugu Mutahi <wm916@uowmail.edu.au>
2021-07-20 15:01:11 +1000

    Added text input for profile details

--- src/screens/settings/EditProfile.tsx
+++ src/screens/settings/EditProfile.tsx
@@ -9,10 +9,24 @@ import {
 } from "@ui-kitten/components";
 import { Icons } from "@greeneggs/core";
 import { StackNavigationProp } from "@react-navigation/stack";
-import { ScrollView, StyleSheet } from "react-native";
+import { ScrollView, StyleSheet, View } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { TextInput } from "react-native-gesture-handler";
+import Svg, { Circle } from "react-native-svg";
 
 const styles = StyleSheet.create({
     header: { padding: 16 },
+    input: {
+        marginTop: -25,
+        marginBottom: -20,
+        backgroundColor: '#f5f5f5',
+        height: 40,
+        borderColor: '#f5f5f5',
+        color: '#191970',
+        fontSize: 14,
+        fontWeight: 'bold',
+        borderWidth: 1
+    },
 });
 
 interface ListItemProps {
@@ -32,6 +46,8 @@ const EditProfileListItem = ({ item }: EProfileListItem) => (
 
 const EditProfile = ({ navigation }: { navigation: StackNavigationProp<any> }) => {
 
+    const insets = useSafeAreaInsets();
+
     const EditProfileItems: ListItemProps[] = [
         {
         },
@@ -46,7 +62,7 @@ const EditProfile = ({ navigation }: { navigation: StackNavigationProp<any> }) =
             <TopNavigation
                 title="Edit Profile"
                 alignment="center"
-                style={{ backgroundColor: "transparent" }}
+                style={{ backgroundColor: "transparent", marginTop: insets.top }}
                 accessoryLeft={() => (
                     <TopNavigationAction icon={Icons.Back} onPress={navigateBack} />
                 )}
@@ -58,6 +74,27 @@ const EditProfile = ({ navigation }: { navigation: StackNavigationProp<any> }) =
                 <Text category="h6" style={styles.header}>
                     Profile Details
                 </Text>
+                <Text category="s1" style={{ padding: 16, color: '#20b2aa' }}>
+                    First Name
+                </Text>
+                <View style={styles.header}>
+                    <TextInput style={styles.input}
+                    />
+                </View>
+                <Text category="s1" style={{ padding: 16, color: '#20b2aa' }}>
+                    Last Name
+                </Text>
+                <View style={styles.header}>
+                    <TextInput style={styles.input}
+                    />
+                </View>
+                <Text category="s1" style={{ padding: 16, color: '#20b2aa' }}>
+                    Bio
+                </Text>
+                <View style={styles.header}>
+                    <TextInput style={styles.input}
+                    />
+                </View>
             </ScrollView>
 
         </>

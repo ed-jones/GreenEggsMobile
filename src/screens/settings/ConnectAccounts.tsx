b9f33b028300293a7ef8760b2b7477ff2e78cc5f
Wambugu Mutahi <wm916@uowmail.edu.au>
2021-07-21 16:51:56 +1000

    Added top navigation

--- src/screens/settings/ConnectAccounts.tsx
+++ src/screens/settings/ConnectAccounts.tsx
@@ -1,6 +1,29 @@
 import React from "react";
-import { Text } from "@ui-kitten/components";
+import { Text, TopNavigation, TopNavigationAction } from "@ui-kitten/components";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { Icons } from "@greeneggs/core";
+import { StackNavigationProp } from "@react-navigation/stack";
 
-const ConnectAccounts = () => <Text>Connect Accounts</Text>;
+const ConnectAccounts = ({ navigation }: { navigation: StackNavigationProp<any> }) => {
+
+    const insets = useSafeAreaInsets();
+
+    const navigateBack = () => {
+        navigation.goBack();
+    };
+
+    return (
+        <>
+            <TopNavigation
+                title="Connect Accounts"
+                alignment="center"
+                style={{ backgroundColor: "transparent", marginTop: insets.top }}
+                accessoryLeft={() => (
+                    <TopNavigationAction icon={Icons.Back} onPress={navigateBack} />
+                )}
+            />
+        </>
+    );
+};
 
 export default ConnectAccounts;

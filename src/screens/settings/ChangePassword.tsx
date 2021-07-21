d3d5ea3959d1946a91f3bf8b99625348d6867991
Wambugu Mutahi <wm916@uowmail.edu.au>
2021-07-21 16:26:19 +1000

    Added all components for changePassword

--- src/screens/settings/ChangePassword.tsx
+++ src/screens/settings/ChangePassword.tsx
@@ -1,6 +1,91 @@
 import React from "react";
-import { Text } from "@ui-kitten/components";
+import { Text, TopNavigation, TopNavigationAction } from "@ui-kitten/components";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { Icons } from "@greeneggs/core";
+import { StackNavigationProp } from "@react-navigation/stack";
+import { Button, ScrollView, StyleSheet, View } from "react-native";
+import { TextInput } from "react-native-gesture-handler";
 
-const ChangePassword = () => <Text>Change Password</Text>;
+
+const styles = StyleSheet.create({
+    header: { padding: 16 },
+    input: {
+        marginTop: -25,
+        marginBottom: -20,
+        backgroundColor: '#f5f5f5',
+        height: 40,
+        borderColor: '#f5f5f5',
+        color: '#191970',
+        fontSize: 13,
+        fontWeight: 'bold',
+        borderWidth: 1
+    },
+    button: {
+        marginTop: 40,
+        color: '#FFFFFF',
+        width: 170,
+        height: 70,
+        fontWeight: 'bold',
+        padding: 16,
+        backgroundColor: '#20b2aa',
+    },
+});
+
+const ChangePassword = ({ navigation }: { navigation: StackNavigationProp<any> }) => {
+
+    const insets = useSafeAreaInsets();
+
+    const navigateBack = () => {
+        navigation.goBack();
+    };
+
+    return (
+        <>
+            <TopNavigation
+                title="Change Password"
+                alignment="center"
+                style={{ backgroundColor: "transparent", marginTop: insets.top }}
+                accessoryLeft={() => (
+                    <TopNavigationAction icon={Icons.Back} onPress={navigateBack} />
+                )}
+            />
+            <ScrollView>
+                <Text category="s1" style={{ padding: 16, color: '#20b2aa', fontSize: 12 }}>
+                    OLD PASSWORD
+                </Text>
+                <View style={styles.header}>
+                    <TextInput style={styles.input}
+                        secureTextEntry={true}
+                    />
+                </View>
+                <Text category="s1" style={{ padding: 16, color: '#20b2aa', fontSize: 12 }}>
+                    NEW PASSWORD
+                </Text>
+                <View style={styles.header}>
+                    <TextInput style={styles.input}
+                        secureTextEntry={true}
+                    />
+                </View>
+                <Text category="s1" style={{ padding: 16, color: '#20b2aa', fontSize: 12 }}>
+                    CONFIRM NEW PASSWORD
+                </Text>
+                <View style={styles.header}>
+                    <TextInput style={styles.input}
+                        secureTextEntry={true}
+                    />
+                </View>
+                <View style={styles.button}>
+                    <Button
+                        color='#FFFFFF'
+                        onPress={() => {
+                            alert('You tapped the button!');
+                        }}
+                        title="Save Changes"
+                    />
+                </View>
+            </ScrollView>
+        </>
+    );
+};
 
 export default ChangePassword;

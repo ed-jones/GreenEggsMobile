8cf7e894da9c564aa71f9ef4f18aabc978fa4910
Wambugu Mutahi <wm916@uowmail.edu.au>
2021-07-21 16:56:51 +1000

    Added top navigation

--- src/screens/settings/Allergies.tsx
+++ src/screens/settings/Allergies.tsx
@@ -1,6 +1,30 @@
 import React from "react";
-import { Text } from "@ui-kitten/components";
+import { Text, TopNavigation, TopNavigationAction } from "@ui-kitten/components";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { Icons } from "@greeneggs/core";
+import { StackNavigationProp } from "@react-navigation/stack";
 
-const Allergies = () => <Text>Allergies</Text>;
+const Allergies = ({ navigation }: { navigation: StackNavigationProp<any> }) => {
+
+    const insets = useSafeAreaInsets();
+
+    const navigateBack = () => {
+        navigation.goBack();
+    };
+
+    return (
+        <>
+            <TopNavigation
+                title="Dietary Preferences"
+                alignment="center"
+                style={{ backgroundColor: "transparent", marginTop: insets.top }}
+                accessoryLeft={() => (
+                    <TopNavigationAction icon={Icons.Back} onPress={navigateBack} />
+                )}
+            />
+        </>
+    );
+};
+
 
 export default Allergies;

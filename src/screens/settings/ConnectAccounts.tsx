25ee3eb17cc6d713df00ccbaa1a4727776c9b4c0
Wambugu Mutahi <wm916@uowmail.edu.au>
2021-08-02 16:00:29 +1000

    Added connect accounts list items

--- src/screens/settings/ConnectAccounts.tsx
+++ src/screens/settings/ConnectAccounts.tsx
@@ -1,16 +1,76 @@
 import React from "react";
-import { Text, TopNavigation, TopNavigationAction } from "@ui-kitten/components";
+import { Divider, Icon, List, ListItem, Text, TopNavigation, TopNavigationAction } from "@ui-kitten/components";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { Icons } from "@greeneggs/core";
 import { StackNavigationProp } from "@react-navigation/stack";
+import { ScrollView, StyleSheet, Switch } from "react-native";
+import Svg, { Circle } from "react-native-svg";
+
+
+
+interface ListItemProps {
+    title: string;
+    icon: string;
+    rightText?: string;
+    color?: string;
+}
+
+interface CAccountsListItem {
+    item: ListItemProps;
+}
+
+const ConnAccountsListItem = ({ item }: CAccountsListItem) => (
+    <>
+        <ListItem
+            title={item.title}
+            accessoryRight={(props) => (
+                <>
+                    <Text category="c2">{item.rightText}</Text>
+                </>
+            )}
+            accessoryLeft={(props) => (
+                <>
+                    <Svg
+                        height="32"
+                        width="32"
+                        style={{ position: "absolute", marginLeft: 12 }}
+                    >
+                        <Circle cx="16" cy="16" r="16" fill={item.color} />
+                    </Svg>
+                    <Icon {...props} name={item.icon} fill="white" />
+                </>
+            )}
+        />
+        <Divider />
+    </>
+);
 
 const ConnectAccounts = ({ navigation }: { navigation: StackNavigationProp<any> }) => {
 
     const insets = useSafeAreaInsets();
 
+    const Connect: ListItemProps[] = [
+        {
+            title: "Facebook",
+            icon: "facebook-outline",
+            color: '#4169e1',
+        },
+        {
+            title: "Google",
+            icon: "google-outline",
+            color: '#ff0000',
+        },
+        {
+            title: "Twitter",
+            icon: "twitter-outline",
+            color: '#87ceeb',
+        },
+    ];
+
     const navigateBack = () => {
         navigation.goBack();
     };
+
 
     return (
         <>
@@ -22,6 +82,9 @@ const ConnectAccounts = ({ navigation }: { navigation: StackNavigationProp<any> 
                     <TopNavigationAction icon={Icons.Back} onPress={navigateBack} />
                 )}
             />
+            <ScrollView>
+                <List data={Connect} renderItem={ConnAccountsListItem} />
+            </ScrollView>
         </>
     );
 };

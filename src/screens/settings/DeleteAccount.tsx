b0601c484d4953878ad762bc05537070db0088d4
Wambugu Mutahi <wm916@uowmail.edu.au>
2021-08-02 16:43:13 +1000

    Added text component

--- src/screens/settings/DeleteAccount.tsx
+++ src/screens/settings/DeleteAccount.tsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { Text, TopNavigation, TopNavigationAction } from "@ui-kitten/components";
+import { Icon, List, ListItem, Text, TopNavigation, TopNavigationAction } from "@ui-kitten/components";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { Icons } from "@greeneggs/core";
 import { StackNavigationProp } from "@react-navigation/stack";
 import { Button, ScrollView, StyleSheet, View } from "react-native";
 import { TextInput } from "react-native-gesture-handler";
+import Svg, { Circle } from "react-native-svg";
 
 const styles = StyleSheet.create({
     header: { padding: 16 },
@@ -30,9 +31,54 @@ const styles = StyleSheet.create({
     },
 });
 
+interface ListItemProps {
+    title: string;
+    icon: string;
+    rightText?: string;
+    color?: string;
+}
+
+interface DAccountsListItem {
+    item: ListItemProps;
+}
+
+const DelAccountsListItem = ({ item }: DAccountsListItem) => (
+    <>
+        <ListItem
+            style={{ padding: 16, backgroundColor: "transparent" }}
+            title={item.title}
+            accessoryRight={(props) => (
+                <>
+                    <Text category="c2">{item.rightText}</Text>
+                </>
+            )}
+            accessoryLeft={(props) => (
+                <>
+                    <Svg
+                        height="32"
+                        width="32"
+                        style={{ position: "absolute", marginLeft: 12 }}
+                    >
+                        <Circle cx="16" cy="16" r="16" fill={item.color} />
+                    </Svg>
+                    <Icon {...props} name={item.icon} fill="red" />
+                </>
+            )}
+        />
+    </>
+);
+
 const DeleteAccount = ({ navigation }: { navigation: StackNavigationProp<any> }) => {
 
     const insets = useSafeAreaInsets();
+
+    const Delete: ListItemProps[] = [
+        {
+            title: "If you delete your account you will lose all recipes and all other personal data. This action cannot be undone. Are you sure?",
+            icon: "alert-triangle-outline",
+            color: '#ffffff',
+        },
+    ];
 
     const navigateBack = () => {
         navigation.goBack();
@@ -49,6 +95,9 @@ const DeleteAccount = ({ navigation }: { navigation: StackNavigationProp<any> })
                 )}
             />
             <ScrollView>
+                <View>
+                    <List data={Delete} renderItem={DelAccountsListItem} />
+                </View>
                 <Text category="s1" style={{ padding: 16, color: '#20b2aa', fontSize: 12 }}>
                     CONFIRM PASSWORD
                 </Text>

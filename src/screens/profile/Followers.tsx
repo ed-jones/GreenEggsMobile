a13606a40c4b50249d5821acb87eb476dd3ee52b
Victor <sharks400@gmail.com>
2021-09-27 18:21:54 +1000

    added Following and Followers file

---
+++ src/screens/profile/Followers.tsx
@@ -0,0 +1,50 @@
+import React, { Key, useContext } from "react";
+import {
+  List,
+  Text,
+  TopNavigation,
+  TopNavigationAction,
+  ListItem,
+  Icon,
+  Divider,
+  withStyles,
+  ThemedComponentProps,
+} from "@ui-kitten/components";
+import { Alert, ScrollView, StyleSheet } from "react-native";
+import { Icons } from "@greeneggs/core";
+import Svg, { Circle } from "react-native-svg";
+import { StackNavigationProp } from "@react-navigation/stack";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import * as SecureStore from "expo-secure-store";
+import { AuthContext } from "@greeneggs/core/auth-context/AuthContext";
+
+const styles = StyleSheet.create({});
+
+const Followers = withStyles(
+  ({
+    navigation,
+    eva,
+  }: { navigation: StackNavigationProp<any> } & ThemedComponentProps) => {
+    const insets = useSafeAreaInsets();
+    const { setToken } = useContext(AuthContext);
+
+    const navigateBack = () => {
+      navigation.navigate("Home");
+    };
+
+    return (
+      <>
+        <TopNavigation
+          title="Followers"
+          alignment="center"
+          style={{ backgroundColor: "transparent", marginTop: insets.top }}
+          accessoryLeft={() => (
+            <TopNavigationAction icon={Icons.Back} onPress={navigateBack} />
+          )}
+        />
+      </>
+    );
+  }
+);
+
+export default Followers;

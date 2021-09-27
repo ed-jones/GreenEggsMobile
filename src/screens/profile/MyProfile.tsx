7d8e4e22f53800a6b535fc0a097ade4eaaa51ff4
Ed Jones <edjones1999@gmail.com>
2021-09-27 17:25:45 +1000

    feat: add pagination to profile recipes

--- src/screens/profile/MyProfile.tsx
+++ src/screens/profile/MyProfile.tsx
@@ -12,10 +12,17 @@ import {
 import { useQuery } from "@apollo/client";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { Alert, Icons, Queries, noavatar } from "@greeneggs/core";
-import { Me, recipes, recipesVariables, Sort } from "@greeneggs/types/graphql";
+import {
+  Me,
+  recipes,
+  recipesVariables,
+  recipes_recipes_data,
+  Sort,
+} from "@greeneggs/types/graphql";
 import LoadingScreen from "../loading/LoadingScreen";
 import RecipeCardSmall from "@greeneggs/core/recipe-card-small";
 import { useNavigation } from "@react-navigation/core";
+import { FlatList } from "react-native-gesture-handler";
 
 const styles = StyleSheet.create({
   avatarContainer: {
@@ -79,18 +86,26 @@ interface MyRecipesProps {
 
 const MyRecipes: FC<MyRecipesProps> = ({ query, userId }) => {
   const navigation = useNavigation();
+  const limit = 2;
+  const [done, setDone] = useState(false);
+  const [recipesState, setRecipesState] = useState<recipes_recipes_data[]>([]);
 
   const myRecipesResult = useQuery<recipes, recipesVariables>(
     Queries.GET_RECIPES,
     {
       variables: {
         offset: 0,
-        limit: 10,
+        limit,
         query: query,
         sort: Sort.NEW,
         filter: {
           user: userId,
         },
+      },
+      onCompleted: (data) => {
+        if (data.recipes.data) {
+          mergeRecipes(data.recipes.data);
+        }
       },
     }
   );
@@ -102,31 +117,62 @@ const MyRecipes: FC<MyRecipesProps> = ({ query, userId }) => {
   if (myRecipesResult.error) {
     return <Alert message="There was an error" type="danger" />;
   }
-  const myRecipes = myRecipesResult.data?.recipes.data;
+
+  if (recipesState === null || recipesState === undefined) {
+    return (
+      <Alert
+        style={{ marginHorizontal: 16 }}
+        message="You haven't uploaded any recipes! Once you've uploaded some recipes they'll be shown here."
+        type="info"
+      />
+    );
+  }
+
+  function mergeRecipes(recipes: recipes_recipes_data[]) {
+    setRecipesState([...recipesState, ...recipes]);
+  }
+
+  async function nextPage() {
+    if (!done) {
+      const result = await myRecipesResult.fetchMore({
+        variables: {
+          offset: recipesState.length,
+          limit,
+          query: query,
+          sort: Sort.NEW,
+          filter: {
+            user: userId,
+          },
+        },
+      });
+      if (result.data.recipes.data) {
+        if (result.data.recipes.data.length === 0) {
+          setDone(true);
+        } else {
+          mergeRecipes(result.data.recipes.data);
+        }
+      }
+    }
+  }
 
   return (
-    <>
-      {myRecipes ? (
-        myRecipes.map((myRecipe) => (
-          <View style={{ marginBottom: 16, marginHorizontal: 16 }}>
-            <RecipeCardSmall
-              recipe={myRecipe}
-              onPress={() =>
-                navigation.navigate("Recipe", {
-                  recipeId: myRecipe.id,
-                })
-              }
-            />
-          </View>
-        ))
-      ) : (
-        <Alert
-          style={{ marginHorizontal: 16 }}
-          message="You haven't uploaded any recipes! Once you've uploaded some recipes they'll be shown here."
-          type="info"
-        />
+    <FlatList
+      onEndReached={() => nextPage()}
+      onEndReachedThreshold={0.5}
+      data={recipesState}
+      renderItem={({ item: myRecipe }) => (
+        <View style={{ marginBottom: 16, marginHorizontal: 16 }}>
+          <RecipeCardSmall
+            recipe={myRecipe}
+            onPress={() =>
+              navigation.navigate("Recipe", {
+                recipeId: myRecipe.id,
+              })
+            }
+          />
+        </View>
       )}
-    </>
+    ></FlatList>
   );
 };
 
@@ -156,58 +202,54 @@ const MyProfile = () => {
 
   return (
     <Layout level="2" style={{ ...styles.view }}>
-      <ScrollView>
-        <TopNavigation
-          style={{ backgroundColor: "transparent", paddingTop: insets.top }}
-          accessoryLeft={() => (
-            <TopNavigationAction
-              icon={Icons.Settings}
-              onPress={() => navigation.navigate("Settings")}
-            />
-          )}
-        />
-        <View style={styles.avatarContainer}>
-          <Pressable onPress={() => navigation.navigate("EditProfilePicture")}>
-            <Avatar
-              style={styles.avatar}
-              shape="round"
-              size="giant"
-              source={me.avatarURI ? { uri: me.avatarURI } : noavatar}
-            />
-          </Pressable>
-        </View>
-        <View style={styles.profileContainer}>
-          <Text category="h5">{`${optional(me.firstName)} ${optional(
-            me.lastName
-          )}`}</Text>
-          <Button
-            size="small"
-            style={styles.button}
-            accessoryLeft={Icons.Edit}
-            onPress={() => navigation.navigate("EditProfile")}
-          >
-            EDIT
-          </Button>
-        </View>
-        <Text style={styles.description} numberOfLines={2}>
-          {optional(me.bio)}
-        </Text>
-        <View style={styles.statContainer}>
-          <ProfileStat label="Following" value={optional(me.followingCount)} />
-          <ProfileStat label="Followers" value={optional(me.followerCount)} />
-          <ProfileStat label="Recipes" value={optional(me.recipeCount)} />
-          <ProfileStat label="Likes" value={optional(me.likeCount)} />
-        </View>
-        <Input
-          placeholder="Search recipes"
-          size="large"
-          style={styles.search}
-          accessoryLeft={Icons.Search}
-          value={myRecipeQuery}
-          onChangeText={(newText) => setMyRecipeQuery(newText)}
-        />
-        <MyRecipes query={myRecipeQuery} userId={me.id} />
-      </ScrollView>
+      <TopNavigation
+        style={{ backgroundColor: "transparent", paddingTop: insets.top }}
+        accessoryLeft={() => (
+          <TopNavigationAction
+            icon={Icons.Settings}
+            onPress={() => navigation.navigate("Settings")}
+          />
+        )}
+      />
+      <View style={styles.avatarContainer}>
+        <Pressable onPress={() => navigation.navigate("EditProfilePicture")}>
+          <Avatar
+            style={styles.avatar}
+            shape="round"
+            size="giant"
+            source={me.avatarURI ? { uri: me.avatarURI } : noavatar}
+          />
+        </Pressable>
+      </View>
+      <View style={styles.profileContainer}>
+        <Text category="h5">{`${me.firstName} ${me.lastName}`}</Text>
+        <Button
+          size="small"
+          style={styles.button}
+          accessoryLeft={Icons.Edit}
+          onPress={() => navigation.navigate("EditProfile")}
+        >
+          EDIT
+        </Button>
+      </View>
+      <Text style={styles.description} numberOfLines={2}>
+        {optional(me.bio)}
+      </Text>
+      <View style={styles.statContainer}>
+        <ProfileStat label="Following" value={me.followingCount.toString()} />
+        <ProfileStat label="Followers" value={me.followerCount.toString()} />
+        <ProfileStat label="Recipes" value={me.recipeCount.toString()} />
+        <ProfileStat label="Likes" value={me.likeCount.toString()} />
+      </View>
+      <Input
+        placeholder="Search recipes"
+        size="large"
+        style={styles.search}
+        accessoryLeft={Icons.Search}
+        value={myRecipeQuery}
+        onChangeText={(newText) => setMyRecipeQuery(newText)}
+      />
+      <MyRecipes query={myRecipeQuery} userId={me.id} />
     </Layout>
   );
 };

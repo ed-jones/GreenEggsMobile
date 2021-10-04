da0c3eae75d3023e070afa7b041729edffc77faa
Ed Jones <edjones1999@gmail.com>
2021-10-04 17:46:18 +1100

    feat: finish diets

--- src/screens/search/filters/FilterRecipeDiets.tsx
+++ src/screens/search/filters/FilterRecipeDiets.tsx
@@ -1,6 +1,6 @@
-import React, { FC, useState } from 'react';
+import React, { FC, useContext, useState } from 'react';
 import { Icons, Queries } from '@greeneggs/core';
-import { List, ListItem, TopNavigation, TopNavigationAction, Text, Input, Divider } from '@ui-kitten/components';
+import { List, ListItem, TopNavigation, TopNavigationAction, Text, Input, Divider, Layout } from '@ui-kitten/components';
 import { useNavigation } from '@react-navigation/core';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { Diets, DietsVariables, Diets_diets_data, RecipeFilter, Sort } from '@greeneggs/types/graphql';
@@ -9,15 +9,39 @@ import LoadingScreen from '../../loading/LoadingScreen';
 import LazyListAlpha from '@greeneggs/core/lazy-alpha-list';
 import { AlphabetType } from '@greeneggs/core/alpha-list';
 import AddToFilter from '../common/add-to-filter';
+import SelectableListItem from '@greeneggs/core/selectable-list-item';
+import { SearchContext } from '@greeneggs/providers/SearchStateProvider';
 
 const FilterRecipeDiets: FC = () => {
   const navigation = useNavigation();
   const insets = useSafeAreaInsets();
   const [query, setQuery] = useState("");
+  const { searchState, setSearchState } = useContext(SearchContext);
+  const [selectedDiets, setSelectedDiets] = useState<string[]>(
+    searchState.filter.diets ?? []
+  );
 
+  const setSelected = (selected: boolean, id: string) => {
+    setSelectedDiets(
+      selected
+        ? [...selectedDiets, id]
+        : [...selectedDiets.filter((diets) => diets !== id)]
+    );
+  };
+
+  const addToFilter = () => {
+    setSearchState?.({
+      ...searchState,
+      filter: {
+        ...searchState.filter,
+        diets: selectedDiets,
+      },
+    });
+    navigation.goBack();
+  };
   
   return (
-    <>
+    <Layout style={{ flex: 1 }} level="2">
       <TopNavigation
         style={{ backgroundColor: "transparent", paddingTop: insets.top }}
         accessoryLeft={() => 
@@ -26,11 +50,11 @@ const FilterRecipeDiets: FC = () => {
             onPress={() => navigation.goBack()}
           />
         }
-        title="Ingredients (Excluded)"
+        title="Diets"
         alignment="center"
       />
       <Input
-        style={{ padding: 16 }}
+        style={{ padding: 16, backgroundColor: 'white' }}
         placeholder="Search Diets"
         accessoryLeft={Icons.Search}
         onChangeText={setQuery}
@@ -45,7 +69,11 @@ const FilterRecipeDiets: FC = () => {
       >
         renderItem={(item) => (
           <>
-            <ListItem title={item.name} />
+            <SelectableListItem
+              title={item.name}
+              selected={selectedDiets.includes(item.id)}
+              setSelected={(selected) => setSelected(selected, item.id)}
+            />
             <Divider />
           </>
         )}
@@ -58,8 +86,12 @@ const FilterRecipeDiets: FC = () => {
         }}
         dataKey="diets"
       />
-      <AddToFilter filterCount={0} />
-    </>
+      <AddToFilter
+        clearFilters={() => setSelectedDiets([])}
+        filterCount={selectedDiets.length}
+        addToFilter={addToFilter}
+      />
+    </Layout>
   );
 }
 

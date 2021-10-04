9b27180e45a71089d0767e8d54081cae8f69d57d
Ed Jones <edjones1999@gmail.com>
2021-10-04 17:44:27 +1100

    feat: finish categories

--- src/screens/search/filters/FilterRecipeCategories.tsx
+++ src/screens/search/filters/FilterRecipeCategories.tsx
@@ -1,22 +1,45 @@
-import React, { FC, useState } from 'react';
+import React, { FC, useContext, useState } from 'react';
 import { Icons, Queries } from '@greeneggs/core';
-import { List, ListItem, TopNavigation, TopNavigationAction, Text, Divider, Input } from '@ui-kitten/components';
+import { Layout, TopNavigation, TopNavigationAction, Divider, Input } from '@ui-kitten/components';
 import { useNavigation } from '@react-navigation/core';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { Categories, CategoriesVariables, Categories_categories_data, RecipeFilter, Sort } from '@greeneggs/types/graphql';
-import { useQuery } from '@apollo/client';
-import LoadingScreen from '../../loading/LoadingScreen';
 import LazyListAlpha from '@greeneggs/core/lazy-alpha-list';
 import { AlphabetType } from '@greeneggs/core/alpha-list';
 import AddToFilter from '../common/add-to-filter';
+import { SearchContext } from '@greeneggs/providers/SearchStateProvider';
+import SelectableListItem from '@greeneggs/core/selectable-list-item';
 
 const FilterRecipeCategories: FC = () => {
   const navigation = useNavigation();
   const insets = useSafeAreaInsets();
   const [query, setQuery] = useState("");
+  const { searchState, setSearchState } = useContext(SearchContext);
+  const [selectedCategories, setSelectedCategories] = useState<string[]>(
+    searchState.filter.categories ?? []
+  );
+
+  const setSelected = (selected: boolean, id: string) => {
+    setSelectedCategories(
+      selected
+        ? [...selectedCategories, id]
+        : [...selectedCategories.filter((categories) => categories !== id)]
+    );
+  };
+
+  const addToFilter = () => {
+    setSearchState?.({
+      ...searchState,
+      filter: {
+        ...searchState.filter,
+        categories: selectedCategories,
+      },
+    });
+    navigation.goBack();
+  };
 
   return (
-    <>
+    <Layout style={{ flex: 1 }} level="2">
       <TopNavigation
         style={{ backgroundColor: "transparent", paddingTop: insets.top }}
         accessoryLeft={() => 
@@ -25,12 +48,12 @@ const FilterRecipeCategories: FC = () => {
             onPress={() => navigation.goBack()}
           />
         }
-        title="Ingredients (Excluded)"
+        title="Categories"
         alignment="center"
       />
       <Input
-        style={{ padding: 16 }}
-        placeholder="Search Allergies"
+        style={{ padding: 16, backgroundColor: 'white' }}
+        placeholder="Search Categories"
         accessoryLeft={Icons.Search}
         onChangeText={setQuery}
         value={query}
@@ -44,7 +67,11 @@ const FilterRecipeCategories: FC = () => {
       >
         renderItem={(item) => (
           <>
-            <ListItem title={item.name} />
+            <SelectableListItem
+              title={item.name}
+              selected={selectedCategories.includes(item.id)}
+              setSelected={(selected) => setSelected(selected, item.id)}
+            />
             <Divider />
           </>
         )}
@@ -57,8 +84,12 @@ const FilterRecipeCategories: FC = () => {
         }}
         dataKey="categories"
       />
-      <AddToFilter filterCount={0} />
-    </>
+      <AddToFilter
+        clearFilters={() => setSelectedCategories([])}
+        filterCount={selectedCategories.length}
+        addToFilter={addToFilter}
+      />
+    </Layout>
   );
 }
 

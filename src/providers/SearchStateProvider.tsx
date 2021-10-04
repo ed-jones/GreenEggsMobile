d5535c135d9e704f7444589cd95373d402a5b443
Ed Jones <edjones1999@gmail.com>
2021-10-04 14:45:09 +1100

    feat: add `SearchStateProvider` component

---
+++ src/providers/SearchStateProvider.tsx
@@ -0,0 +1,43 @@
+import React, { useState, FC, createContext } from 'react';
+import { RecipeFilter, Sort } from '@greeneggs/types/graphql';
+
+interface SearchState {
+  query?: string;
+  filter: RecipeFilter;
+  sort: Sort;
+}
+
+const defaultSearchState: SearchState = {
+  query: undefined,
+  filter: {
+    ingredients: undefined,
+    categories:undefined,
+    allergies: undefined,
+    diets: undefined,
+    cookTime: undefined,
+    user: undefined,
+  },
+  sort: Sort.RELEVANT,
+}
+
+interface ISearchContext {
+  searchState: SearchState,
+  setSearchState?: (searchState: SearchState) => void,
+}
+
+export const SearchContext = createContext<ISearchContext>({
+  searchState: defaultSearchState,
+  setSearchState: undefined 
+});
+
+const SearchStateProvider: FC = ({ children }) => {
+  const [searchState, setSearchState] = useState<SearchState>(defaultSearchState);
+
+  return (
+    <SearchContext.Provider value={{searchState, setSearchState}}>
+      {children}
+    </SearchContext.Provider>
+  );
+};
+
+export default SearchStateProvider;

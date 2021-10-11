acbf4cede68d0ded31ced5695142486a077eeac3
Ed Jones <edjones1999@gmail.com>
2021-10-11 22:00:20 +1100

    feat: add categories as a list

--- src/screens/home/categories.tsx
+++ src/screens/home/categories.tsx
@@ -1,18 +1,14 @@
-import React, { FC } from 'react';
-import { Queries } from '@greeneggs/graphql';
-import { CategoriesVariables, Categories_categories_data, RecipeFilter, Sort, Categories as CategoriesType } from '@greeneggs/types/graphql';
-import { Background, LazyList } from '@greeneggs/ui';
-import { Text } from '@ui-kitten/components';
-
-interface CategoryItemProps {
-  category: Categories_categories_data
-}
-
-const CategoryItem: FC<CategoryItemProps> = ({ category }) => {
-  return (
-    <Text>{category.name}</Text>
-  );
-}
+import React, { FC } from "react";
+import { Queries } from "@greeneggs/graphql";
+import {
+  CategoriesVariables,
+  Categories_categories_data,
+  RecipeFilter,
+  Sort,
+  Categories as CategoriesType,
+} from "@greeneggs/types/graphql";
+import { Background, Icons, LazyList } from "@greeneggs/ui";
+import { ListItem } from "@ui-kitten/components";
 
 export const Categories: FC = () => {
   return (
@@ -26,15 +22,15 @@ export const Categories: FC = () => {
       >
         query={Queries.GET_CATEGORIES}
         variables={{
-          query: '',
+          query: "",
         }}
         dataKey="categories"
         emptyMessage="No categories found."
         errorMessage="No categories found."
         renderItem={({ item: category, index }) => (
-          <CategoryItem category={category} key={index.toString()} />
+          <ListItem title={category.name} accessoryRight={Icons.Forward} />
         )}
       />
     </Background>
   );
-}
+};

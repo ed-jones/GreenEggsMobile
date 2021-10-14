98976a322cd7c4f867130d1fc0fc815a3013312b
Ed Jones <edjones1999@gmail.com>
2021-10-14 20:00:40 +1100

    fix: select background colour

--- src/ui/select.tsx
+++ src/ui/select.tsx
@@ -2,7 +2,7 @@ import React, { FC } from 'react';
 import { Select as UIKittenSelect, SelectProps } from "@ui-kitten/components";
 
 export const Select: FC<SelectProps> = ({ children, style, ...props }) => (
-  <UIKittenSelect style={[style, { backgroundColor: 'white' }]} {...props}>
+  <UIKittenSelect style={[style]} {...props}>
     {children}
   </UIKittenSelect>
 );

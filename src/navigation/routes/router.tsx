3a073648568207c57021ecb5f95c9185ecdca204
Ed Jones <edjones1999@gmail.com>
2021-10-15 15:01:52 +1100

    feat: Add loading state to authentication

--- src/navigation/routes/router.tsx
+++ src/navigation/routes/router.tsx
@@ -1,23 +1,42 @@
-import React, { FC, useContext } from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { useQuery } from '@apollo/client';
-import { Queries } from '@greeneggs/graphql';
-import { Me } from '@greeneggs/types/graphql';
+import React, { FC, useState, ReactNode } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { useQuery } from "@apollo/client";
+import { Queries } from "@greeneggs/graphql";
+import { Me } from "@greeneggs/types/graphql";
+import { LoadingScreen } from "@greeneggs/screens";
 
-import { Stack } from '../stack';
-import { LoggedInRoutes } from './logged-in-routes';
-import { LoggedOutRoutes } from './logged-out-routes';
+import { Stack } from "../stack";
+import { LoggedInRoutes } from "./logged-in-routes";
+import { LoggedOutRoutes } from "./logged-out-routes";
 
+enum SessionStates {
+  LOADING,
+  LOGGED_IN,
+  LOGGED_OUT,
+}
+
+const SESSION_STATE_ROUTE_MAP: Record<SessionStates, ReactNode> = {
+  [SessionStates.LOADING]: <Stack.Screen name="Loading" component={LoadingScreen} />,
+  [SessionStates.LOGGED_IN]: LoggedInRoutes,
+  [SessionStates.LOGGED_OUT]: LoggedOutRoutes,
+};
 
 export const Router: FC = () => {
-  const { data } = useQuery<Me>(Queries.ME);
-  const me = data?.me.data;
+  const [sessionState, setSessionState] = useState<SessionStates>(
+    SessionStates.LOADING
+  );
+  useQuery<Me>(Queries.ME, {
+    onCompleted: (data) =>
+      Boolean(data?.me.data)
+        ? setSessionState(SessionStates.LOGGED_IN)
+        : setSessionState(SessionStates.LOGGED_OUT),
+  });
 
   return (
     <NavigationContainer>
       <Stack.Navigator headerMode="none">
-        {Boolean(me) ? LoggedInRoutes: LoggedOutRoutes}
+        {SESSION_STATE_ROUTE_MAP[sessionState]}
       </Stack.Navigator>
     </NavigationContainer>
-  )
-}
+  );
+};

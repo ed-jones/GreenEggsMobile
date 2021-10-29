90030e63109496ca7d6acfd18ead054cf9d2d1f8
Ed Jones <edjones1999@gmail.com>
2021-10-29 18:56:42 +1100

    new commit

--- scripts/updategraphql.sh
+++ scripts/updategraphql.sh
@@ -3,7 +3,7 @@
 # Update the schema from the API.
 echo "Updating schema.json from API..."
 yarn apollo client:download-schema \
-	--endpoint http://localhost:4000 schema.json
+	--endpoint https://api.greeneggs.app/ schema.json
 
 # Generate the typescript typedefs.
 echo "Generating typescript type definitions..."
